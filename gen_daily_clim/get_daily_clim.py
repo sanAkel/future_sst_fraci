96c369357c5d12e5a5b039cbd234ab106902441b
sanAkel <santha.akella@nasa.gov>
2024-01-02 22:43:50 -0500

    Add short usage doc

--- gen_daily_clim/get_daily_clim.py
+++ gen_daily_clim/get_daily_clim.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# Usage:
+# source g5_modules
+# ./get_daily_clim.py
+# --
+
 import pandas as pd
 import os
 
@@ -9,7 +14,7 @@ to_exec = path_to_bin + "cal_daily_clim.x"
 start_year, end_year = [2007, 2023]
 
 # some arbitrary year
-dummy_dates = pd.date_range(start='1/1/1900', end='12/1/1900', freq='D')
+dummy_dates = pd.date_range(start='12/2/1900', end='12/31/1900', freq='D')
 #dummy_dates = pd.date_range(start='1/1/1900', end='12/31/1900', freq='D')
 
 for dd in range( len(dummy_dates)):
