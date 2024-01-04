fc9d16e773b4a962492ae16e113fbfac519e3442
sanAkel <santha.akella@nasa.gov>
2024-01-03 23:57:54 -0500

    Handle both daily climatology calculation and extract daily data

--- gen_daily_clim/get_daily_clim.py
+++ gen_daily_clim/get_daily_clim.py
@@ -8,23 +8,46 @@
 import pandas as pd
 import os
 
+#which_exec = 'daily_clim'
+which_exec = 'extract_daily'
+# --
+
 path_to_bin = "/discover/nobackup/sakella/" + "geosMom6/v11.1.1/GEOSgcm/build/bin/"
-to_exec = path_to_bin + "cal_daily_clim.x"
+#~~~~~~~~~~~~~
+if (which_exec == 'daily_clim'):
+  to_exec = path_to_bin + "cal_daily_clim.x"
+  start_year, end_year = [2007, 2023] # for climatology
+# some arbitrary dates
+  dummy_dates = pd.date_range(start='1/1/1900', end='12/31/1900', freq='D') # for climatology
+else:
+  to_exec = path_to_bin + "extract_daily_sst_ice.x"
+# x0049 (summer)
+  dummy_dates = pd.date_range(start='7/1/2023', end='10/1/2023', freq='D')
+#~~~~~~~~~~~~~
 
-start_year, end_year = [2007, 2023]
-
-# some arbitrary year
-dummy_dates = pd.date_range(start='12/2/1900', end='12/31/1900', freq='D')
-#dummy_dates = pd.date_range(start='1/1/1900', end='12/31/1900', freq='D')
-
-for dd in range( len(dummy_dates)):
-    mon, day = [dummy_dates[dd].month, dummy_dates[dd].day]
-    # print(mon, day)
-    cmd = to_exec + ' ' +\
-          '{} {}'.format(start_year, end_year) + ' ' +\
-          '{} {}'.format(mon, day)
-    #print(cmd)
-    exit_code = os.system(cmd)
-    if (exit_code != 0):
-      print("Error running:")
-      sys.exit(cmd)
+if (which_exec == 'daily_clim'):
+  for dd in range( len(dummy_dates)):
+     mon, day = [dummy_dates[dd].month, dummy_dates[dd].day]
+     # print(mon, day)
+     cmd = to_exec + ' ' +\
+           '{} {}'.format(start_year, end_year) + ' ' +\
+           '{} {}'.format(mon, day)
+     #print(cmd)
+     exit_code = os.system(cmd)
+     if (exit_code != 0):
+       print("Error running:")
+       sys.exit(cmd)
+else:
+  for dd in range( len(dummy_dates)):
+     year, mon, day = [dummy_dates[dd].year, dummy_dates[dd].month, dummy_dates[dd].day]
+     # print(year, mon, day)
+     cmd = to_exec + ' ' +\
+           '{} {} {} .true.'.format(year, mon, day)
+     print(cmd)
+     exit_code = os.system(cmd)
+     if (exit_code != 0):
+       print("Error running:")
+       sys.exit(cmd)
+     else:
+       os.system('/bin/mv *.nc  data/')
+       os.system('/bin/mv *.bin data/')
