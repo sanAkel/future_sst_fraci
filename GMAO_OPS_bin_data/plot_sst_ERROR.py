953edd38ca2afe1b05870a446b9f9d645efeaf81
sanAkel <santha.akella@nasa.gov>
2024-01-03 20:35:21 -0500

    Add climatolgical difference

--- GMAO_OPS_bin_data/plot_sst_ERROR.py
+++ GMAO_OPS_bin_data/plot_sst_ERROR.py
@@ -21,25 +21,48 @@ my_mask = land_sea_mask.land_mask.values
 #plt.pcolormesh(ds_geos.lon.values, ds_geos.lat.values, sst_masked), plt.colorbar(shrink=0.5)
 #plt.show()
 
-# Difference from persistence
-dss = xr.open_mfdataset("data/" + "sst_fraci_*.nc4")
+data_path_clim = "/discover/nobackup/projects/gmao/advda/sakella/future_sst_fraci/data/ncFiles/"
+data_path_real = "/discover/nobackup/projects/gmao/advda/sakella/future_sst_fraci/GMAO_OPS_bin_data/data/"
 
-nT = dss.time.shape[0]
-dSST_mean = np.zeros((nT,), dtype=np.float64)
-dSST_rmse = np.zeros_like( dSST_mean)
+ds_real = xr.open_mfdataset(data_path_real + "sst_fraci_*.nc4")
+ds_clim = xr.open_mfdataset(data_path_clim + "daily_clim_mean_sst_fraci_000101*.nc", concat_dim='time', combine='nested', use_cftime=True)
+
+nT = ds_real.time.shape[0]
+mean_pers = np.zeros((nT,), dtype=np.float64)
+rmse_pers = np.zeros_like( mean_pers)
+
+mean_clim = np.zeros_like( mean_pers)
+rmse_clim = np.zeros_like( mean_pers)
 # There is no point in working with ice, since coverage varies everyday!
-for id in range(0, nT):
-  daily_sst = dss.isel(time=id).SST.values
-  sst_masked=apply_mask(daily_sst, my_mask)
-  if (id==0):
-    sst0 = sst_masked
-  dsst = (sst_masked - sst0).flatten()
-  # unweighted.
-  dSST_mean[id], dSST_rmse[id] = [np.nanmean(dsst, dtype=np.float64), np.nanstd(dsst, dtype=np.float64)]
 
-plt.figure( figsize=(8, 6))
-plt.subplot(111)
-plt.plot(range(0, nT), dSST_mean, 'b.-', label='mean (error)')
-plt.plot(range(0, nT), dSST_rmse, 'k.-', label='RMSE (error)')
+for id in range(0, nT):
+  daily_sst_real = ds_real.isel(time=id).SST.values
+  daily_sst_clim = ds_clim.isel(time=id).SST.values
+
+  real_sst_masked=apply_mask(daily_sst_real, my_mask)
+  clim_sst_masked=apply_mask(daily_sst_clim, my_mask)
+
+  # Difference from persistence
+  if (id==0):
+    sst0 = real_sst_masked
+
+  dsst_pers = (real_sst_masked - sst0).flatten()
+  dsst_clim = (real_sst_masked - clim_sst_masked).flatten()
+
+  # unweighted.
+  mean_pers[id], rmse_pers[id] = [np.nanmean(dsst_pers, dtype=np.float64), np.nanstd(dsst_pers, dtype=np.float64)]
+  mean_clim[id], rmse_clim[id] = [np.nanmean(dsst_clim, dtype=np.float64), np.nanstd(dsst_clim, dtype=np.float64)]
+
+plt.figure( figsize=(8, 4))
+plt.subplot(121)
+plt.plot(range(0, nT), mean_pers, 'b.-', label='Persistence')
+plt.plot(range(0, nT), mean_clim, 'g.-', label='Climatology')
 plt.legend()
+plt.title('Global mean error ($^\circ$K)')
+
+plt.subplot(122)
+plt.plot(range(0, nT), rmse_pers, 'b.-', label='Persistence')
+plt.plot(range(0, nT), rmse_clim, 'g.-', label='Climatology')
+#plt.legend()
+plt.title('RMSE ($^\circ$K)')
 plt.show()
